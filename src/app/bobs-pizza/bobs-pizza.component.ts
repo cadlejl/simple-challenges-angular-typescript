39b44a733a52a8436967e4a6ddf5f003db6b42cb
Jaime Cadle <jaime@coficiency.com>
2018-05-24 21:30:11 -0500

    Add subtotal functionality

--- src/app/bobs-pizza/bobs-pizza.component.ts
+++ src/app/bobs-pizza/bobs-pizza.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { Pizza, Crust, Topping } from './model';
 
 @Component({
@@ -24,7 +24,7 @@ import { Pizza, Crust, Topping } from './model';
         .check-radio {
             margin: 4px;
         }
-        .yourOrder {
+        .orderTotal {
             font-size: 1.5rem;
         }
         p {
@@ -38,71 +38,93 @@ export class BobsPizzaComponent {
     private yourPizza: string;
     private yourCrust: string;
     private yourToppings: string[] = [];
-    private tList:string[]  = [];
+    private toppingList:string[]  = [];
+    private toppingTotal: number = 0;
+    private sizePrice: number = 0;
+    private crustPrice: number = 0;
+    private subtotal: number = 0;
+
+    sizeCrustChange($event:any) {
+        let val = $event.value;
+        let price = val.price;
+
+        if(val.size) this.sizePrice = price;
+        else/*(val.type)*/ this.crustPrice = price;
+
+        this.setSubtotal(this.sizePrice, this.crustPrice, this.toppingTotal);
+    }
+
+    setSubtotal(sizePrice:number, crustPrice:number, toppingTotal:number) {
+        this.subtotal = sizePrice + crustPrice + toppingTotal;
+    }
 
     chooseToppings($event: any) {
+        let val = $event.source.value;
+        let topping: string = val.type;
+        let toppingPrice: number = val.price;
         
-        let topping: string = $event.source.value.type;
-        
-        if($event.checked) this.tList.push(topping);
+        if($event.checked) {
+            this.toppingTotal += toppingPrice;
+            this.toppingList.push(topping);
+        }
         else {
-            this.tList.forEach(element => {
+            this.toppingTotal -= toppingPrice;
+            this.toppingList.forEach(element => {
                 if(element.includes(topping)) {
-                    let tRemove = this.tList.indexOf(element);
-                    this.tList.splice(tRemove,1);
+                    let tIndex = this.toppingList.indexOf(element);
+                    this.toppingList.splice(tIndex,1);
                 }
             });
         }
-
-        this.configureToppingString(this.tList);
+        
+        //this.checkSpecial(this.toppingList);
+        this.setSubtotal(this.sizePrice, this.crustPrice, this.toppingTotal);
+        this.configureToppingString(this.toppingList);
     }
     
     configureToppingString(tList:string[]) {
-
         let t:string[] = [];
-        let tListLast:number = this.tList.length - 1;
-        let tLast:number = t.length-1;
+        let tListLast:number = this.toppingList.length - 1;
 
        if(tList.length === 1) t[0] = (' ' + tList[0] + '!!');
 
        if(tList.length === 2){
            t[0] = (' ' + tList[0] + ',');
            t[1] = (' and ' + tList[1] + '!!');
-       };
+       }
 
-       if(tList.length > 2){
-
+       if(tList.length > 2) {
            t[0] = (' ' + tList[0] + ',');
            
            for(let i = 1; i < tListLast; i++) {
                t[i] = (' ' + tList[i] + ',');
-            }
-            
-            t[tListLast] = (' and ' + tList[tListLast] + '!!');
-       }
-        
+           }
+           
+           t[tListLast] = (' and ' + tList[tListLast] + '!!');
+       };
+
         this.yourToppings = t;
     }
 
+    // checkSpecial(toppingList: string[]) {
+    //     let t = toppingList;
+    //     let t2:string;
+    //     let tt = t.includes(t2);
+    //     if(
+    // }
 
-
-
-
-
-
-
-    private pizzas: Pizza[] = [
+    pizzas: Pizza[] = [
         { size: 'Baby Bob Size (10")', price: 10 },
         { size: 'Mama Bob Size (13")', price: 13 },
         { size: 'Papa Bob Size (16")', price: 16 }
     ]
 
-    private crusts: Crust[] = [
+    crusts: Crust[] = [
         { type: "thin crust", price: null },
         { type: "deep dish", price: 2 }
     ]
 
-    private toppings: Topping[] = [
+    toppings: Topping[] = [
         { type: "pepperoni", price: 1.5 },
         { type: "onions", price: 0.75 },
         { type: "green peppers", price: 0.50 },
